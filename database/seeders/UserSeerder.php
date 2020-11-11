35cad837f35f64908df8af070fc7d74cb8c4603f
Yela <ekaman@yahoo.com>
2020-11-12 00:54:09 +0800

    Database make migrate table and db seeding

---
+++ database/seeders/UserSeerder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Contracts\Container\BindingResolutionException;
+
+class UserSeerder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        DB::table('users')->insert([
+            'name' => "alfaa",
+            'email' => 'alfa@yahoo.com',
+            'password' => Hash::make('12345lll')
+        ]);
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Users;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('login');
 });
+
+Route::get('users',[Users::class,'index']);
+// Route::get('users','Users@index'); 
